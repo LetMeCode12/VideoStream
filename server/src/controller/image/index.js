4849d7f7229a4fa6af0cdc1cf72257f941c68ef6
LetMeCode12 <norbertskulski@wp.pl>
2022-06-10 16:02:38 +0200

    aa

--- server/src/controller/image/index.js
+++ server/src/controller/image/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path'
+import fetch from "node-fetch";
+import {pipeline} from 'node:stream';
+import {promisify} from 'node:util'
+
 const imageController = express.Router();
+const streamPipeline = promisify(pipeline);
 
 
 imageController.get("/image/:fileName",(req,res)=>{
@@ -9,6 +14,13 @@ imageController.get("/image/:fileName",(req,res)=>{
     
     const file = fs.readFileSync(path.join(process.cwd(),"files","images",fileName))
     res.json({buffer:file,fileName})
+});
+
+imageController.get("/test2", async (req,res)=>{
+    const request = await fetch("http://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=json");
+    const xd = request.body
+    res.status(308)
+    xd.pipe(res)
 });
 
 export default imageController;
